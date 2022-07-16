1d0c77f737d38370ccb5567a6676a9747547af8a
Yanulio <yagshulepa@edu.hse.ru>
2022-07-16 20:52:21 +0300

    Added images for README.md file

---
+++ cmake-build-debug/CMakeFiles/GraphData.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/User/Desktop/git/GraphDataStorage/DataConverter.cpp" "CMakeFiles/GraphData.dir/DataConverter.cpp.obj" "gcc" "CMakeFiles/GraphData.dir/DataConverter.cpp.obj.d"
+  "C:/Users/User/Desktop/git/GraphDataStorage/DataInputAndOutput.cpp" "CMakeFiles/GraphData.dir/DataInputAndOutput.cpp.obj" "gcc" "CMakeFiles/GraphData.dir/DataInputAndOutput.cpp.obj.d"
+  "C:/Users/User/Desktop/git/GraphDataStorage/GraphStruct.cpp" "CMakeFiles/GraphData.dir/GraphStruct.cpp.obj" "gcc" "CMakeFiles/GraphData.dir/GraphStruct.cpp.obj.d"
+  "C:/Users/User/Desktop/git/GraphDataStorage/main.cpp" "CMakeFiles/GraphData.dir/main.cpp.obj" "gcc" "CMakeFiles/GraphData.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/GraphData.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GraphData.dir/DataConverter.cpp.obj"
+  "CMakeFiles/GraphData.dir/DataConverter.cpp.obj.d"
+  "CMakeFiles/GraphData.dir/DataInputAndOutput.cpp.obj"
+  "CMakeFiles/GraphData.dir/DataInputAndOutput.cpp.obj.d"
+  "CMakeFiles/GraphData.dir/GraphStruct.cpp.obj"
+  "CMakeFiles/GraphData.dir/GraphStruct.cpp.obj.d"
+  "CMakeFiles/GraphData.dir/main.cpp.obj"
+  "CMakeFiles/GraphData.dir/main.cpp.obj.d"
+  "GraphData.exe"
+  "GraphData.exe.manifest"
+  "GraphData.pdb"
+  "libGraphData.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GraphData.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
